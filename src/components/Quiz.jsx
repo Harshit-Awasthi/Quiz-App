60b00171fc8857bfa917a517349d616c2249ba8a
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-06-10 13:16:08 +0530

    "Shuffled asnwers and added Quiz complete image at the end"

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz()
 {
@@ -7,6 +8,8 @@ export default function Quiz()
     const [userAnswers,setUserAnswers]=useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+
+    const quizIsComplete=activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer)
     {
@@ -16,6 +19,19 @@ export default function Quiz()
             }
         );
     }
+
+    if(quizIsComplete) 
+        {
+            return (
+                <div id="summary">
+                    <img src={quizCompleteImg} alt="Trophy icon" />
+                    <h2>Quiz Completed!</h2>
+                </div>
+            )
+        }
+
+        const shuffledAnswers= [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(()=>Math.random()-0.5);
 
     return (
 
@@ -27,7 +43,7 @@ export default function Quiz()
             {QUESTIONS[activeQuestionIndex].text}
         </h2>
         <ul id="answers">
-            {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            {shuffledAnswers.map((answer) => (
                 <li key={answer} className="answer">
                 <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
                 </li>
